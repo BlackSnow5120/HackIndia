0604fffd54e44e73479c2fa015ff648b48f9ec4e
Bhuvain Jhamb <124060979+BlackSnow5120@users.noreply.github.com>
2024-10-07 14:24:07 +0530

    aaaa

--- migrations/1_initial_migration.js
+++
@@ -1,5 +0,0 @@
-const PlagiarismCheck = artifacts.require("PlagiarismCheck");
-
-module.exports = function(deployer) {
-    deployer.deploy(PlagiarismCheck);
-};
--- plagiarism-checker/contracts/PlagiarismCheck.sol
+++ plagiarism-checker/contracts/PlagiarismCheck.sol
@@ -1,20 +1,20 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-
-contract PlagiarismCheck is Ownable {
+contract PlagiarismCheck {
+    
     struct Document {
-        string hash;
-        address author;
-        uint timestamp;
+        string hash; // Hash of the document
+        address author; // Address of the document's author
+        uint timestamp; // Timestamp of document upload
     }
 
     mapping(string => Document) public documents;
+    
     event DocumentAdded(string hash, address author, uint timestamp);
-
-    function addDocument(string memory _hash) public onlyOwner {
+    
+    function addDocument(string memory _hash) public {
         require(documents[_hash].author == address(0), "Document already exists");
+        
         documents[_hash] = Document(_hash, msg.sender, block.timestamp);
         emit DocumentAdded(_hash, msg.sender, block.timestamp);
     }
@@ -22,7 +22,7 @@ contract PlagiarismCheck is Ownable {
     function checkPlagiarism(string memory _hash) public view returns (bool) {
         return documents[_hash].author != address(0);
     }
-
+    
     function getDocumentDetails(string memory _hash) public view returns (address, uint) {
         require(documents[_hash].author != address(0), "Document does not exist");
         return (documents[_hash].author, documents[_hash].timestamp);
